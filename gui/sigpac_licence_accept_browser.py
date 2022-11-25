b472864111826d1bb314df61c7528f64e88b46fa
Francisco Pérez Sampayo <francisco.perez@geomatico.es>
2022-11-25 12:54:24 +0100

    Obtiene cookie scrapeando la web del fega y ya no hace falta cargar la web.

--- gui/sigpac_licence_accept_browser.py
+++ gui/sigpac_licence_accept_browser.py
@@ -1,15 +1,14 @@
 import urllib.request
 import os
 from qgis.PyQt.QtWidgets import (QDialog, QVBoxLayout, QLabel, QDialogButtonBox, QLineEdit)
-from qgis.PyQt.QtCore import QUrl
-from qgis.PyQt.QtWebKitWidgets import QWebView
+from qgis.PyQt.QtCore import QUrl, pyqtSignal
+from qgis.PyQt.QtWebKitWidgets import QWebView, QWebInspector
 from qgis.PyQt.QtWebKit import QWebSettings
-from urllib.request import urlopen, Request
 
-# import sys
-# from PyQt5.QtWidgets import QApplication
 
 class SigPacLicenceAcceptBrowser(QDialog):
+    conditionsAccepted = pyqtSignal(str)
+
     def __init__(self, parent = None):
         super().__init__(parent)
         self.resize(800, 800)
@@ -25,15 +24,23 @@ class SigPacLicenceAcceptBrowser(QDialog):
         # Widget para el navegador
         self.webview = QWebView(self)
         self.webview.page().networkAccessManager().finished.connect(self.network_request_done)
-        #self.webview.loadStarted.connect(self.loaded)
 
         webSettings = self.webview.settings()
         webSettings.clearMemoryCaches()
 
         self.webview.settings().setAttribute(QWebSettings.PluginsEnabled, True)
+        # self.webview.settings().setAttribute(QWebSettings.DeveloperExtrasEnabled, True)
+        self.webview.settings().setAttribute(QWebSettings.JavascriptEnabled, True)
+
+        # inspector = QWebInspector()
+        # inspector.setPage(self.webview.page())
+        # inspector.setVisible(True)
+
+
         self.webview.load(QUrl(self.url))
 
         layout.addWidget(self.webview)
+        # layout.addWidget(inspector)
 
         self.setLayout(layout)
         self.setWindowTitle("Acepte las condiciones")
@@ -42,4 +49,5 @@ class SigPacLicenceAcceptBrowser(QDialog):
     def network_request_done(self, reply):
         if reply.url().toString() == 'https://www.fega.gob.es/atom/es.fega.sigpac.xml':
             self.cookie = reply.request().rawHeader(b'Cookie')
+            self.conditionsAccepted.emit(self.cookie)
             self.accept()
